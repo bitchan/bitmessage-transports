5ca8c07e0deeaad45dd876cc8b8639e234a0239d
Kagami Hiiragi <kagami@genshiken.org>
2015-02-25 12:38:21 +0300

    Extract transports from bitmessage

---
+++ tests/tcp-node.js
@@ -0,0 +1,17 @@
+var TcpTransport = require("../lib").TcpTransport;
+
+function start() {
+  var server = new TcpTransport({port: 22333});
+  // In node 0.12/io 1.0 we can use {host: x, port: y} syntax so it'll
+  // be more compatible with the ws transport options.
+  server.listen(22333, "127.0.0.1");
+  server.on("connection", function(client) {
+    client.on("message", function(command, payload) {
+      if (command === "echo-req") {
+        client.send("echo-res", payload);
+      }
+    });
+  });
+}
+
+start();
